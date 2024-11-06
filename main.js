efc372e905be8617bfba820db11b3d9354a2b020
Tasi <tasi87@gmail.com>
2024-11-06 15:09:40 +0100

    odstranil som zbytocny kus kodu

--- main.js
+++ main.js
@@ -5,25 +5,25 @@ import moment from 'moment'
 
 console.log(moment().startOf('hour').fromNow());
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
 
 /**
 * createNewCard vytvorí nový card element
@@ -44,12 +44,3 @@ function createKnotCard(name, description) {
 }
 
 const cardContainer = document.querySelector('.cards ul');
-
-// --------------
-// Main code
-// --------------
-
-data.forEach(card => {
-  
-  createKnotCard(card.name, card.description);
-});
