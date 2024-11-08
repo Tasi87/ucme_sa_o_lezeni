62e4daddad778b5e014e2cdc32667150a6783a83
Tasi <tasi87@gmail.com>
2024-11-08 09:24:48 +0100

    pridane dalsie dva elementy do newCard

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,19 @@
 * createNewCard vytvorí nový card element
 * do Stringu budem posielať Názov uzlu a popis uzlu
 *
+* @param {String} category
 * @param {String} name
 * @param {String} description
+* @param {String} image
 */
-function createKnotCard(name, description) {
+function createKnotCard(category, name, description, image) {
     const newCard = document.createElement('li');
   
     newCard.innerHTML = `
+	  <h2>${category}</h2>
       <h3>${name}</h3>
       <p>${description}</p>
+	  <a href"#">${image}</a>
     `;
   
     cardContainer.appendChild(newCard);
