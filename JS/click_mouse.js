9a20968d6c78601a7c777ef3bf29881ff62e9fc1
Tasi <tasi87@gmail.com>
2024-11-08 13:53:57 +0100

    naprogramoval som animaciu po kliknuti, kde sa meni obrazok s textom.

--- JS/click_mouse.js
+++ JS/click_mouse.js
@@ -3,10 +3,31 @@ const ul = document.querySelector('ul');
 
 cardContainer.addEventListener('click', (event) => {
 	const clickedItem = event.target.closest('li');
+
 	if (clickedItem) {
+		const h2 = clickedItem.querySelector('h2');
+		const h3 = clickedItem.querySelector('h3');
+		const p = clickedItem.querySelector('p');
 		const img = clickedItem.querySelector('img');
-		if (img) {
-			img.style.display = img.style.display === 'none' ? 'block' : 'none';
+
+		const isImageVisible = img && img.style.display === 'block';
+		
+		if (isImageVisible) {
+			img.style.display = 'none';
+			if (h2) h2.style.display = 'block';
+			if (h3) h3.style.display = 'block';
+			if (p) p.style.display = 'block';
+		} else {
+			if (img) img.style.display = 'block'
+			if (h2) h2.style.display = 'none';
+			if (h3) h3.style.display = 'none';
+			if (p) p.style.display = 'none';
 		}
+
+		clickedItem.classList.add('animate__animated', 'animate__flipInY', 'animate__faster');
+
+		clickedItem.addEventListener('animationend', () => {
+			clickedItem.classList.remove('animate__animated', 'animate__flipInY', 'animate__faster');
+		}, { once: true});
 	}
 });
